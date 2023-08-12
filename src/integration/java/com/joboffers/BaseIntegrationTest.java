1f2384c41ce71553c5c4aaa6fa3128f2c43ca926
kuba <kuba200213@gmail.com>
2023-08-12 11:51:22 +0200

    base integration tests and integration package

---
+++ src/integration/java/com/joboffers/BaseIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.joboffers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = JobOffersSpringBootApplication.class)
+public class BaseIntegrationTest {
+}
